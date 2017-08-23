71e74d710c73c49fa28c8170cf1fb5a17e387f45
Jarad Niemi <niemi@iastate.edu>
2017-08-23 09:30:22 -0500

    Add revenue documentation

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Revenue data
+#'
+#' Revenue data for crop portion of watersheds, i.e. it does not account for the
+#' fact that some of the watersheds have a portion that is prairie. For details
+#' on how these revenue values were obtained, see the xlsx file in the data-raw
+#' folder.
+#'
+#' @format A \code{data.frame} with 3 variables: watershed, year, and revenue
+#'
+#' @seealso \code{\link{pnas_data}}
+"revenue"
